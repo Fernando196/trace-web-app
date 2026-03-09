36216d6b66e8cfd36ff7c6008f3c482e0d0f5e9e
Fernando196 <fer_jose1996@hotmail.com>
2026-03-08 23:13:25 -0500

    feat: implement route animation and marker reference management in map component

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,23 +1,70 @@
 <script setup lang="ts">
-import Map from '~/components/app/Map.vue'
+import type { LatLngExpression } from 'leaflet'
+import MapLeaflet from '~/components/app/Map.vue'
 import MarkerEvent from '~/components/app/MarkerEvent.vue'
 import SidebarMap from '~/components/app/SidebarMap.vue'
 
 const eventStore = useEventStore()
-const { events } = storeToRefs(eventStore)
+const { events, selectedEvent } = storeToRefs(eventStore)
+
+const visibleCoords = ref<LatLngExpression[] | []>([])
+let interval: number
+const markerRefs = new Map<string | number, any>()
+
+const setMarkerRef = (el: any, id: string | number) => {
+  if (el) markerRefs.set(id, el)
+}
+
+const animateRoute = () => {
+  if (interval) clearInterval(interval)
+  visibleCoords.value = []
+  let i = 0
+
+  const coords = selectedEvent.value?.coords
+  if (!coords?.length) return
+
+  interval = setInterval(() => {
+    if (i < coords.length) {
+      visibleCoords.value = [...(visibleCoords.value || []), coords[i]!]
+      i++
+    } else {
+      clearInterval(interval)
+    }
+  }, 50)
+}
+
+watch(
+  () => selectedEvent.value,
+  () => {
+    animateRoute()
+    const targerMarker = markerRefs.get(selectedEvent.value?.id!)
+    if (targerMarker) {
+      targerMarker.handleChangePopup(true)
+    }
+  }
+)
 </script>
 
 <template>
   <div class="h-full flex">
     <sidebar-map class="overflow-x-hidden" />
     <ClientOnly class="flex-1">
-      <Map>
+      <MapLeaflet>
         <MarkerEvent
           v-for="event in events"
           :key="'marker-event' + event.id"
           :event="event"
+          @click="eventStore.updateSelectedEvent(event)"
+          :ref="(el) => setMarkerRef(el, event.id)"
         />
-      </Map>
+        <LPolyline
+          v-if="visibleCoords?.length"
+          :lat-lngs="visibleCoords"
+          color="#b6e56e"
+          :weight="5"
+          :opacity="1"
+        />
+      </MapLeaflet>
     </ClientOnly>
   </div>
 </template>
