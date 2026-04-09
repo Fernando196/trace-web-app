a235140a0552e6a4d2c70b1643c687b1c33e375c
Fernando196 <fer_jose1996@hotmail.com>
2026-04-08 23:07:28 -0500

    feat: add CheckpointIcon, FlagIcon, and WaterIcon components for enhanced icon support

---
+++ app/components/common/icons/FlagIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M3 3v18h2V3H3zm3 3c0-.83.67-1.5 1.5-1.5h11c.83 0 1.5.67 1.5 1.5v6c0 .83-.67 1.5-1.5 1.5h-11c-.83 0-1.5-.67-1.5-1.5V6z"
+    />
+  </svg>
+</template>
---
+++ app/components/common/icons/WaterIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446A9 9 0 1 1 12 2.25c0 .276.007.55.02.821zm0 0c-.132 0-.263 0-.393 0a7.5 7.5 0 0 0-7.92 12.446A9 9 0 1 0 12 21.75c0-.276-.007-.55-.02-.821z"
+    />
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112z"
+    />
+  </svg>
+</template>
